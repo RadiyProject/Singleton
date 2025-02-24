f1fb9b8a367d60e813bdb44756c3f870662f3910
RadiyX <sigor00@mail.ru>
2025-02-24 21:10:08 +0700

    normalization realized

--- Models/Dataset.cs
+++ Models/Dataset.cs
@@ -18,7 +18,7 @@ public class Dataset {
         return result;
     }
 
-    public static List<int> TextToNumber(List<string> values) {
+    public static List<double> TextToDouble(List<string> values) {
         List<string> distincts = [.. values.Distinct()];
         Dictionary<string, int> labels = [];
         int i = 0;
@@ -27,10 +27,38 @@ public class Dataset {
             i++;
         }
 
-        List<int> numbers = [];
+        List<double> numbers = [];
         foreach (string value in values)
             numbers = [.. numbers, labels[value]];
 
         return numbers;
+    }
+
+    public static List<double>? StringifiedNumbersToDouble(List<string> values) {
+        List<double> numbers = [];
+        foreach (string value in values)
+            if (double.TryParse(value, out double parsed))
+                numbers = [.. numbers, parsed];
+            else
+                return null;
+
+        return numbers;
+    }
+
+    public static double[] Normalize(double[] values) {
+        double min = values[0];
+        double max = values[0];
+        foreach (double value in values) {
+            if (value < min)
+                min = value;
+            if (value > max)
+                max = value;
+        }
+
+        double subtracted = max - min;
+        for (int i = 0; i < values.Length; i++)
+            values[i] = (values[i] - min) / subtracted;
+
+        return values;
     }
 }
