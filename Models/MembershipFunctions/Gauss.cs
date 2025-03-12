94476ce91d714969c2f4cb6a7cb647185faa7873
RadiyX <sigor00@mail.ru>
2025-03-13 02:15:00 +0700

    Gauss function fixed

--- Models/MembershipFunctions/Gauss.cs
+++ Models/MembershipFunctions/Gauss.cs
@@ -6,7 +6,7 @@ public class Gauss(float leftBorder, float rightBorder, int areasCount) : Member
     {
         CheckBorders(borders, "функции Гаусса");
 
-        float center = (borders[1] - borders[0]) * 0.5f;
+        float center = (borders[1] - borders[0]) * 0.5f + borders[0];
         float sigma = (borders[1] - center) * 0.333334f;
         return (float)Math.Exp(-(value - center) * (value - center) * 0.5f / (sigma * sigma));
     }
